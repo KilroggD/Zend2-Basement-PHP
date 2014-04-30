2d47a4258eb058741ebe206b855cffcb8ea76a82
v.kopychev <v.kopychev@alekongroup.ru>
2014-04-30 16:14:16 +0400

    acl updater from config

---
+++ module/Acl/src/Acl/Service/PermissionUpdateService.php
@@ -0,0 +1,16 @@
+<?php
+namespace Acl\Service;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of PermissionUpdateService
+ * Сервис для обновления пермишнов в базе
+ * @author kopychev
+ */
+class PermissionUpdateService {
+    //put your code here
+}
